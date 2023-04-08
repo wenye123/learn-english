fd94cad6a014b5db0fddb054e8057d6e4b06799e
wenye <1583247975@qq.com>
2023-04-09 02:50:21 +0800

    更新

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 ## 介绍
 
-学习英语语法等
+学习英语
+- 语音(pronunciation)
+- 词汇(vocabulary)
+- 语法(grammar)
---
+++ 英语兔/1.英语语法体系.js
@@ -0,0 +1,105 @@
+
+语法的目的就是为了造句
+
+
+词性(词类): 10大词类
+  1. 名词(nouns): 表人和物
+    1. 可数名词: 前面需要加数词
+      a apple
+    2. 不可数名词: 前面加量词
+      a cup of water
+  2. 动词(verbs): 啥句子成分都能干
+    1. 谓语动词: 表示主语动作或状态，有时要搭配助动词
+      1. 特点
+        1. 时态： 16种时态(时间*装填)
+          1. 动作时间
+            现在 
+            过去
+            将来
+            过去将来
+          2. 动作状态
+            一般
+            完成
+            进行
+            完成进行
+        2. 假设，情感，语气
+          1. 虚拟语气
+          2. 陈述语气
+          3. 祈使语气
+      2. 分类 -- 刚好对应5种基本句式
+        1. 不及物动词 -- 主语+动词
+        2. 及物动词 -- 主语+动词+宾语
+        3. 双及物动词 -- 主语+动词+间接宾语+直接宾语
+        4. 复杂及物动词 -- 主语+动词+宾语+宾补
+        5. 系动词 -- 主语+动词+表语
+      3. 助动词 
+        1. be(am is are was were been) -- 表示进行时态，被动语态等
+        2. do(does did done) -- 表示疑问或否定语气
+        3. have(have had) -- 表示完成时态
+        4. can(might must) -- 表情态
+      第三人称单数(she/he/it)后面动词需要加上s/es
+    2. 非谓语动词: 不能单独做谓语，在句子中主要充当各种补足成分(表语 宾补 定语 状语等)，几乎能取代所有从句从而简化句子
+      1. 动词不定式: 通常是to+动词原型，主要充当 主语 宾语 表语 定语 状语等
+        To study is important for students
+      2. 动名词: 本质上就是名词，通常以动词原型+ing，主要充当 主语 宾语 表语 定语 状语等
+        I enjoy reading books
+      3. 分词: 充当定语 状语等
+        现在分词
+          The boy watching the movie is my brother
+        过去分词
+          The book written by Mark Twain is a classic
+  2. 冠词(articles): 说明人和物
+  3. 代词(pronouns): 代替人和物
+  4. 形容词(adjectives): 形容人与物
+  5. 数词(numerals): 表数量
+  6. 副词(adverbs): 修饰动词或形容词
+  7. 介词(prepositions): 表示和其他词关系的词
+  8. 叹词(interjections): 表感叹
+  9. 连词(conjunctions): 连接词与句
+
+句子成分: 8种句子成分
+  1. 主语
+  2. 谓语动词
+  3. 宾语
+  4. 宾语补语
+  5. 主语补语/表语
+  6. 定语: 主要用来修饰主语或宾语
+    The little white rabbit ate a large carrot
+  7. 状语: 主要用来修饰谓语动词
+    The rabbit ate quickly
+  8. 同位语: 主要用来把主语和宾语再说一遍
+    wenye, a programer, write code
+
+句子
+  1. 简单句(simple sentences) -- 什么 + 怎么样
+    - 主语(subject) + 谓语(predicate)
+    - 人物 + 动作/发生了什么事
+        动作(动词verbs): 8种基本句型
+          1. 可以独立完成的动作: 主语 + 不及物动词
+            wenye is sleeps
+          2. 有一个动作的承受者: 主语 + 单及物动词 + 宾语(object)
+            wenye likes you
+          3. 有2个动作承受者: 主语 + 双及物动词 + 间接宾语(indirect object) + 直接宾语(direct object)
+            wenye teaches you english
+          4. 有一个动作承受者(但需补充): 主语 + 复杂及物动词 + 宾语 + 宾语补足语
+            wenye considers you smart
+          5. 非动作: 主语 + 系动词 + 主语补语(表语)
+            系动词： 表示联系 就是将后面的补语赋值给前面主语
+            wenye is talk
+            wenye smells nice
+          6. 第5种(主系表)的倒装: there be句型
+            there is wenye
+          7. 第1种(主语+谓语动词)的延伸: 主语 + 谓语动词 + 状语
+            I live in china 
+          8. 第4种(主语+谓语动词+宾语+宾补)的延伸: 主语 + 谓语动词 + 宾语 + 状语
+            I put carrot on the table
+  2. 并列复合句(compound sentence) -- 句子的简单叠加 并列关系
+    i like code, and you like design
+  3. 主从复合句(complex sentence) -- 句子之间从属关系
+    名词性从句
+      简单句做主语 -- 主语从句
+      简单句做宾语 -- 宾语从句
+      简单句做表语 -- 表语从句
+      简单句做同位语 -- 同位语从句
+    简单句做定语 -- 定语从句
+    简单句做状语 -- 状语从句
