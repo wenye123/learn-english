cfcdc613e5ed9881ddadaa640190977ebd9e44eb
wenye <1583247975@qq.com>
2023-04-09 16:31:52 +0800

    更新

---
+++ 英语兔/2.谓语动词的时态.js
@@ -0,0 +1,80 @@
+1. 现在
+  1. 一般现在时 -- 这兔子吃胡萝卜
+    1. 语法
+      1. 动词原型
+         eat carrots
+      2. 动词原型+s(第三人称单数)
+        she eats carrots
+    2. 特点
+      1. 表达事实
+        The sun rises in the east
+        I eat a carrot -- 没状态就很怪异
+      2. 表达习惯
+        I play basketball
+      3. 表示预计发生的事
+        The bus leaves at 8PM tonight
+  2. 现在进行时 -- 这兔子正在吃胡萝卜
+    1. 助动词be的变位(am/is/are) + 动词的现在分词
+      I am eating a carrot;
+  3. 现在完成时 -- 这兔子已经吃完胡萝卜
+    1. 助动词have的变位(have,has)+动词的过去分词
+      I have eaten a carrots;
+  4. 现在完成进行时 -- 这兔子现在已经吃完了一些胡萝卜
+    1. 助动词have的变位(have,has)+been(be的过去分词)+动词的现在分词
+      I have been eating a carrot;
+2. 过去
+  1. 一般过去时 -- 这兔子过去吃胡萝卜
+    1. 动词过去式(不等于过去分词)
+      I ate a carrot;
+  2. 过去进行时 -- 这兔子昨天下午3点吃胡萝卜
+    助动词be过去式的变位(was,were) + 动词的现在分词
+      I was eating a carrot;
+  3. 过去完成时 -- 这兔子昨天下午3点时已经吃完胡萝卜
+    助动词have过去式的变位(had) + 动词的过去分词
+      I had eaten 5 carrots for lunch yestoday;
+  4. 过去完成进行时 -- 这兔子昨天下午3点时已经吃完了一些胡萝卜
+    助动词have过去式的变位(had) + been + 动词的现在分词;
+      I had been eating a carrot;
+3. 将来 -- will也可以用shall 有微妙区别
+  1. 一般将来时 -- 这兔子将来吃胡萝卜
+    1. 语法
+      1. will + 动词原型 -- 这里是确定要发生的事情，不太确定的得用语气
+        I will eat a carrot;
+    2. 其他表示将来时态
+      1. 一般现在时表将来
+        The bus leaves at 8PM;
+      2. 助动词be的变位+going to+动词原型 / going也可以省略 / about取代going
+        I am going to eat a carrot;
+        I am to eat a carrot;
+        I am about to eat a carrot;
+  2. 将来进行时 -- 这兔子明天下午3点会正在吃胡萝卜
+    1. will + be + 动词的现在分词
+      I will be eating a carrot;
+  3. 将来完成时 -- 这兔子明天下午3点时已经吃完胡萝卜
+    1. will + have + 动词的过去分词
+      I will have eaten a carrots;
+  4. 将来完成进行时 -- 这兔子明天下午3点时已经吃完了一些胡萝卜
+    1. will + have + been(be的过去分词)+动词的现在分词
+      I will have been eating a carrot;
+3. 过去将来(常用语过去时态的从句中) -- 将来时态的will改成would
+  1. 一般过去将来时 -- 下个月的某一天 兔子会吃胡萝卜
+    1. 语法
+      1. would + 动词原型
+        I knew you would watch my video;
+    2. 其他表示将来时态
+      1. 助动词be的变位(was were)+going to+动词原型 / going也可以省略 / about取代going
+        I was going to eat a carrot;
+        I was to eat a carrot;
+        I was about to eat a carrot;
+  2. 过去将来进行时 -- 下个月的某一天 兔子正在吃胡萝卜
+    1. would + be + 动词的现在分词
+      I said I would be eating a carrot at 1PM tomorrow;
+  3. 过去将来完成时 -- 下个月的某一天 兔子已经吃完胡萝卜
+    1. would + have + 动词的过去分词
+      I said I would have eaten a carrots by 1Pm tomorrow;
+  4. 过去将来完成进行时 -- 下个月的某一天 兔子已经吃完了一些胡萝卜
+    1. would + have + been(be的过去分词)+动词的现在分词
+     I said I would have been eating a carrot by 1Pm tomorrow;
+
+
+
---
+++ 英语兔/3.谓语动词的语气.js
@@ -0,0 +1,61 @@
+1. 陈述语气
+  the rabbit ate the carrot;
+2. 祈使语气
+  1. 用第二人称+一般现在时，去掉第二人称并保持动词原型
+    you eat the carrot => eat the carrot
+    you dont eat it = > dont eat it;
+    you are quiet! => be quiet;
+3. 虚拟语气
+  1. 表不可能的假设
+    1. 表示和现在事实相反
+      1. 语法:
+        条件句(动词过去式)[注意be要变成were无关人称] + 主句(would+动词原型)
+      2. 例子
+        if I see the rabbit now, i will bite him; // 错误
+        if I saw the rabiit now, I would bite him;// 正确 
+        if I were you, I would give this video a coin; // 正确
+    2. 表示和将来事实相反(或可能性很小)
+      1. 语法
+        条件句(should/were to +动词原型)[注意这里should是shall的过去式不表示应该，shall也能和will一样做将来助动词] + 主句(would+动词原型)
+      2. 例子
+        if I should see the rabbit tomorrow, i will bite him; // 错误
+        if I should see the rabbit tomorrow, i would bite him; // 正确
+    3.  表示和过去时间相反 
+      1. 语法
+        条件句(had+动词过去分词) + 主句(would have + 动词过去分词)
+      2. 例子
+        if I saw the rabbit a few days ago, I would bite him; // 错误
+        if I had seen the rabbit a few days ago, I would have bitten him; // 正确
+  2. 表愿望 请求 建议等
+    1. wish/hope/would rather等
+      1. 对现在的愿望
+        1. 语法
+          wish + 动词过去式(be的话变成were无关人称)
+        2. 例子
+          I wish that I were a rabbit; -- that可省略
+          I would rather that you didnt eat the carrot; // 我宁愿你不会吃(还没吃)
+      2. 对将来的愿望
+        1. 语法 
+          wish + should/might/could/would + 动词原型
+        2. 例子
+          I wish I could eat the carrot;
+      3. 对过去的愿望 
+        1. 语法
+          wish + had/would have + 动词过去分词
+        2. 例子
+          I wish I had eaten the carrot;
+          I would rather that you hadnt eaten the carrot; // 我宁愿你没吃(已经吃了)
+    2. demand/suggest/insist/order等 -- 动词时态 + should+动词原型(should可以省略)
+      1. 表示对现在愿望
+        I demand that you should give me a carrot;
+      2. 表示对将来愿望(视频没说 自己想的)
+        I will demand that you should give me a carrot;
+      3. 表示对过去愿望
+        I demanded that you should give me a carrot;
+4. 疑问语气
+  1. 陈述语气+前置助动词
+    Did the rabbit eat the carrot?
+5. 条件语气
+  if I see the rabbit tomorrow, i will bite hime;
+6. 感叹语气
+  What a beautiful day
