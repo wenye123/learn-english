e4edd22005a44623a3e9280b7932a55134aca62c
wenye <1583247975@qq.com>
2023-04-12 23:10:25 +0800

    更新

---
+++ 英语兔/6.谓语动词-助动词&情态动词.js
@@ -0,0 +1,133 @@
+1. 解释
+  1. 辅助助动词
+  2. 不能单独做谓语动词
+  3. 一个句子中可以包含多个助动词
+2. 种类
+  1. 基本助动词 -- 做助动词没有实际含义
+    1. be(am is are was were been)
+      1. 做助动词 表示进行时态，被动语态等
+      1. 作系动词 表赋值
+      2. 做实义动词 表存在
+        to be or not to be, that is a question;
+    2. have(has had)
+      1. 做助动词 表示完成时态
+      2. 做实义动词，表示有
+    3. do(does did done)
+      1. 做助动词
+        1. 表强调
+          the rabbit does like coins; 
+        2. 表否定
+          the rabbit does not like wolf;
+        3. 表疑问
+          do you like apple
+  2. 情态助动词 -- 做助动词有实际含义
+    1. can/could
+      1. 做助动词
+        1. 表示有能力/会/可以
+          i can kill a wolf;
+        2. 表请求或许可(could比can更加诚恳)
+          can/could I borrow you book;
+        3. 表可能性(could have done还能表虚拟语态)
+          anything can happen;
+          the rabbit could have gone home earlier, but he didnt finish work on time; -- could have done 表示过去本可以做但是没有做
+      2. can做实义动词 表示把...放进罐头
+    2. may/might 
+      1. 做助动词
+        1. 表请求或许可(might比may更礼貌)
+          may/might I borrow you book;
+        2. 表可能性(可能性小于can/could)
+          the rabbit isnt here he may be at home;
+        3. 表祝愿 
+          may you success! -- 祝你成功
+    3. must
+      1. 做助动词
+        1. 表必须
+          you must finish your homework first;
+        2. +not表禁止
+          you must not smoke here; -- 表必须 不是不必须而是必须不
+        3. 表可能性/推测
+          the light is on, the rabbit must be at home; 
+    4. will/would -- 构成将来时 表请求建议或表推测(此时不构成将来时)
+      1. 做助动词
+        1. 构成将来时
+          the rabbit will eat a carrot;
+        2. 表请求 
+          will you lend me that book; -- 表请求
+          would you please subscribe to my channel; -- 英语一般用would you please表请求
+        3. 表可能性/推测 
+          ask him, he will/would know;
+    5. shall/should  
+      1. 做助动词
+        1. 构成将来时 同will
+          I shall/will be there at 9AM;
+        2. shall
+          1. 表请求或征求意见
+            shall we meet at 9AM;
+          2. 表警告 命令
+            you shall not pass;
+        3. should
+          1. 表应该
+            you should treat the rabiit well;
+          2. 表推测 
+            It should rain tomorrow;
+          3. 表竟然
+            It is surprising that he should be late;
+          4. 构成虚拟语气(一般出现在条件状语从句中)
+            If it should rain tomorrow, I wouldnt go;
+            you should have eaten the carrot;
+    6. ought to 
+      1. 做助动词
+        1. 表应该(语气比should强)
+          you ought to treat the rabiit well;
+    7. dare/dared 
+      1. 做助动词
+        1. 表敢
+          the rabbit dare not tell the truth to the wolf;
+      2. 做实义动词
+        1. 表敢 挑战 冒险
+    8. need 
+      1. 做助动词
+        1. 表需要 有必要
+          the rabbit wait;
+          the rabbit need not wait; -- 表不 后面直接跟not wait
+      2. 做实义动词 表需要
+        I need your coins;
+        the rabbit needs to wait; -- 做谓语动词 后接动词不定式
+        the rabbit does not need to wait; -- 表不 此时是谓语动词 前面加助动词 does not
+    9. used to  
+      1. 做助动词
+        1. 表过去的动作 强调和现在对比
+          the rabbit used to eat apples, now he only eats carrots;
+      2. 注意 
+        和use,be used to (somthing/doing something)没关系
+          could I use your phone
+  3. 半助动词
+    1. be able to -- 能够
+      1. 不同于can表示现在和过去的能力 可表将来的能力
+        i will be able to reach school on time;
+      2. was/were able to.. 表示成功做成某事
+        I was able to reach school on time;
+    2. be unable to -- 不能
+    3. be to -- 被命令做某事 注定
+      you are to finish home work first;
+      I think they are fall in love with each other; -- 注定相爱
+    4. be going to -- 将要 打算
+      I am going to eat dinner at 6PM today;
+    5. be about to -- 将要
+    6. had better -- 最好
+      you had better follow my orders;
+    7. be due to -- 应该 
+    8. be likely to -- 可能
+    9. be meant to -- 意味着
+    10. be obliged to -- 被迫
+    11. be supposed to -- 应该
+    12. be willing to -- 愿意
+    13. be unwilling to -- 不愿意
+    14. have to -- 必须
+    15. seem to -- 似乎
+    16. 。。。
+
+
+
+
+
