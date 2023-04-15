731d0c8b3bf800538be8d4aa06d29c97d1cfa1db
wenye <1583247975@qq.com>
2023-04-15 23:14:00 +0800

    更新

--- 英语兔/1.英语语法体系.js
+++ 英语兔/1.英语语法体系.js
@@ -14,6 +14,17 @@
       - 有些动词后面不能接不定式做宾语
   - 判断非谓语动词
   - 判断是8种句型的哪一个
+词性变化
+  名词变名词
+    名词+动名词
+      english speaking;
+  动词变动词
+    动词+介词 = 动词短语
+      check out;
+  形容词变名词
+    1. the + 形容词 
+      the rich; -- 富人
+
 
 
 词性(词类): 10大词类
@@ -128,13 +139,71 @@
       4. can(will, might must should) -- 表情态 也称为情态动词
       5. 半助动词(be able to...)
 2. 冠词(articles): 说明人和物
-  3. 代词(pronouns): 代替人和物
-  4. 形容词(adjectives): 形容人与物
-  5. 数词(numerals): 表数量
-  6. 副词(adverbs): 修饰动词或形容词
-  7. 介词(prepositions): 表示和其他词关系的词
-  8. 叹词(interjections): 表感叹
-  9. 连词(conjunctions): 连接词与句
+  1. 不定冠词
+    1. 发音: a用在辅音前 an用在元音前
+    2. 用法
+      1. 泛指用法
+        1. 泛指某一个人或物
+          I saw a rabbit; -- 我看到兔子(a表示泛指 中文常翻译成我看到一只兔子)
+        2. 泛指某一类(最好用零冠词复数)
+          A rabbit is smarter than a wolf; -- 兔子比狼聪明
+      2. 表示数字1
+        I gave you a hundred carrots;
+          I gave you one hundred carrots;
+      3. 表示每
+        I eat 10 carrots a day;
+          I eat 10 carrots every day;
+      4. 主谓一致
+        a teacher and video creator;
+      5. 固定表达 
+        have a good time; -- 玩得开心
+        as a result; -- 因此;结果
+        in a hunrry; -- 匆忙的
+        all of a sudden; -- 突然
+  2. 定冠词
+    1. 发音: [de]用在辅音前 [di]用在元音前
+    2. 用法
+      1. 特指
+        1. 再次提到的人或物
+          I saw a rabbit, the rabbit ran fast;
+        2. 用在序数词和形容词的最高级前
+          the first rabbit;
+          the biggest carrot;
+        3. 独一无二的名词前
+          the sun; the moon; the earth;
+      2. 泛指(最好用零冠词复数)
+        I like playing the piano; -- 一般西洋古典乐器才这么用
+        The computer has changed our way of life;
+      3. the+形容词 表示某一类人(能将形容词转成名词)
+        the rich; the poor;
+      4. 固定表达
+        all the time; -- 一直
+        by the way; -- 顺便
+        out of the question; -- 没门
+  3. 零冠词
+    1. 泛指
+      1. 复数泛指(不可数名词不加复数形式)
+        rabbits are smart;
+        computers have changed our way of life;
+        water is important;
+      2. 泛指概念
+        summer is hot, winter is cold;
+        I like play basketball;
+        I like geography;
+        is spring; onMonday;
+      3. 专有名词
+        papa rabbit;
+      4. 固定表达
+        from time to time; -- 不时地
+        face to face; -- 面对面地
+        out of question; -- 毫无疑问
+3. 代词(pronouns): 代替人和物
+4. 形容词(adjectives): 形容人与物
+5. 数词(numerals): 表数量
+6. 副词(adverbs): 修饰动词或形容词
+7. 介词(prepositions): 虚词 表示和其他词关系的词
+8. 叹词(interjections): 虚词 表感叹
+9. 连词(conjunctions): 虚词 连接词与句
 
 句子成分: 8种句子成分
   1. 主语
@@ -184,12 +253,4 @@
       简单句做同位语 -- 同位语从句
     简单句做定语 -- 定语从句/关系从句/形容词从句
     简单句做状语 -- 状语从句
-      1. 表时间
-      2. 表地点
-      3. 表条件
-      4. 表让步
-      5. 表方式
-      6. 表比较
-      7. 表原因
-      8. 表目的
-      9. 表结果
+      表时间 表地点 表条件 表让步 表方式 表比较 表原因 表目的 表结果
---
+++ 英语兔/8.介词.js
@@ -0,0 +1,243 @@
+介词有点类似编程语言中的class 合成的短语可以充当N多种成分
+  动词+介词=短语动词，更像中文的成语 含义很难确定 需要记忆
+
+
+1. 解释
+  1. 介词(附加信息(时间/空间/方式/逻辑)) + 介词宾语(承受对象，可以是多种词类) = 介词短语
+2. 构成
+  1. 简单介词
+    1. 常见介词
+      in: 在。。。之内
+      on: 在。。。上面
+      at: 在。。。处
+      。。。
+    2. 技巧: 本义和引申义
+      about
+        本义: 在。。。外面
+        引申义: 一般都是由空间性的含义引申出来
+          关于; 
+            I konw something about the rabbit; -- 我知道关于这个兔子的事儿;
+          大约;
+            there are about 20 carrots; -- 大约有20根胡萝卜;
+          针对; 
+            lets do something about the problem; -- 让我们做些事儿解决问题;
+          某人某事有某种特性;
+            theres something dangerous about the wolf; -- 狼感觉有点危险
+          到处(在外面来来回回)
+            stop runing about; -- 别来来回回跑;
+  2. 短语介词(不单独表完整含义)
+    by way of: 通过。。。方式
+    at the end of: 在。。。的最后
+    along with: 和。。。一起
+    because of: 因为。。。
+    previous to: 。。。之前
+    。。。
+  3. 合成介词
+    into: 进入。。。之内
+    onto: 在。。。之上
+    throughout: 遍及。。。的每一个部分
+    upon: 在。。。之上
+    within: 在。。。之内
+    notwithstanding: 尽管
+    。。。
+  4. 分词介词(动词的分词构成)
+    including: 包括。。。
+    regarding: 关于。。。, 至于。。。
+    considering: 鉴于。。。, 考虑到。。。
+    given: 考虑到。。。, 如果。。。
+    。。。
+3. 常见英语介词用法
+  1. 表空间
+    1. in: 在。。。里面
+      in the house; in the park; in the country;
+      there are some birds in the tree; -- 树上有一些鸟
+    2. on: 在。。。上面(有接触)
+      on the table; on the floor;
+      there are some apples on the tree; -- 表示长在树上的
+    3. at: 在某处
+      at the corner of the street;
+      the bus stopped at the bus station;
+    4. over: 
+      1. 覆盖(有接触); 
+        the monther put a blanket over the child;
+      2. 在。。。上方(无接触) 还特指正上方; 
+        the bridge over the river;
+        the lamp over the table;
+      3. 越过上空
+        the plane flew over the mountain;
+        the rabbit climbed over the fence;
+    5. above: 在。。。上方(有点类似over，但不需要特指正上方，而且不像over能表示动态)
+      the sky above us;
+    6. under: 在。。。下面(和over相对)
+      there is a ball under the desk;
+    7. below: 在。。。下方(和above相对)
+      the rabbit dived below the surface of the water; -- 兔子潜入了水下
+    8. near: 在。。。周边;在。。。附近(往往有些距离)
+      near the hospital;
+      there is a school near here;
+    9. by/beside: 在。。。旁边
+      there is a hotel by/beside the river;
+    10. next to: 靠近 紧挨着
+      the building next to the school;
+    11. across: 从表面穿过(类似on的表面)
+      go across the street;
+      swim across the river;
+    12. through: 从内部穿过
+      go through the tunnel;
+      walk through the forest;
+      look through the window;
+    13. pass: 经过 路过
+      the rabbit walked past the wolf without saying any words;
+    14. in front of: 在。。。前方
+      the rabbit is standing in front of the wolf;
+    15. before: 在。。。之前(一般不表示空间 除非表达在众人/观众前)
+      she played the violin before the crowd; -- 她在众人前拉小提琴
+    16. behind: 在。。。之后(和 in front of相对)
+      the wolf is standing behind the rabbit;
+    17. between: 在。。。之间(常和and一起用 表示在。。。与。。。之间),也能表达多者之间
+      the carrot is between the rabbit and the wolf;
+      you should not eat between meals; -- 你不应该在正餐之间吃东西;
+    18. among: 在。。。之间(一般指三者或更多人物之间)
+      the rabbit is standing among the carrots;
+    19. round/around: 围绕 环绕
+      trees round/around the field; -- 环绕田地四周的树
+      around the world; -- 全世界
+    20. along: 沿着，顺着
+      walk along this road -- 沿着这条路一直走
+    21. to: 向;朝。。。
+      go to school;
+    22. toward/towards: 朝着。。。的方向(类似to)
+      walk toward home; -- 朝家走
+    23. onto: 到。。。上面
+      the rabbit jumped onto the table;
+    24. into: 到。。。里面(内部)
+      walk into the house;
+    25. out of: 从里面出来
+      get out of the car;
+    26. opposite: 在。。。对面
+      our school is opposite my house; -- 我们的学校在我家对面
+    27. against:
+      1. 与。。。相反;
+        we were rowing against the current; -- 我们逆水行舟
+      2. 倚靠
+        the rabbit was leaning against the tree;
+    28. up: 沿。。。向上
+      up the hill; -- 上山
+      up the river; -- 沿河流逆流而上
+    29. donw: 沿。。。向下
+      walk donw the road; -- 沿着这条路走下去
+      roll down the hill; -- 从山上滚下去
+    30. off: 离。。。有一定距离
+      school lies off home;
+  2. 表时间 
+    1. in: 表示某个时间范围内 (时间范围(in>on>at))
+      in 2030; in winter; in december;
+    2. on: 表示具体某一天
+      on Monday; on christmas day;
+    3. at: 表示具体的某个点
+      at noon; at 6 oclock; at dawn;
+    4. during: 持续; 在。。。期间
+      during the past 2 days; -- 在过去的2天里
+    5. for: 因。。。持续
+      I have been learning english for 2 years; -- 我持续学习英语两年时间
+    6. since: 自从
+      since last week -- 自从上周以来
+    7. before: 在。。。(时间点)之前
+      before Monday -- 星期一之前
+    8. after: 在。。。之后(和before相对)
+      after two years
+    9. from。。。to。。。: 从。。。到。。
+      from 9:00 to 10:00
+    10. by: 不迟于。。前(和before相比 更侧重于不超过这条时间线)
+      by 2 oclock -- 在2点之前
+      by tomorrow -- 在明天之前
+    11. until: 直到。。。为止
+      util 8 oclcok;
+  3. 表方式 
+    1. in: 。。。的方式
+      lets talk in english;
+    2. through: 通过。。。方式;
+      we succeed through hard work;
+    3. by: 通过。。。
+      we learn english by watching videos;
+      I go to work by card;
+    4. with: 用。。。;和。。。一起
+      we write with pens;
+    5. on: 在。。。上面(接电子通讯设备比较多)
+      we talked on the phone;
+  4. 表原因
+    1. for: 因为。。。;由于。。。
+      this rabbit is famous for being able to speak; -- 这只兔子因会说话出名
+    2. with: 。。。是和。。。分不开的原因
+      the rabbit is shaking with cold; -- 兔子冷的发抖
+    3. because of/ due to/ owing to/ on account of: 因为。。。
+      the rabbit didnt go out because of the rain;
+        和because的区别: because是连词连接从句
+  5. 表关于
+    1. about: 关于
+      could you tell me somthing about your school;
+    2. of: 此外的; 附加的
+      he thought of something important -- 他想起了一些重要的事情
+    3. on: 关于 
+      wolud you like some advice on learning english; -- 你想要些关于学习英语的建议吗
+  6. 表数值
+    1. at: 在某处
+      at a high price;
+      at a fast rate
+    2. by: 后接度量标准
+      they get paid by the hour; -- 他们按小时拿报酬
+    3. for: 以。。。的价钱
+      I bought these books for 100 Yuan;
+  7. 表状态
+    1. in: 在。。。状态中 
+      in progress; -- 在进行状态中
+      in a dilemma; -- 处于进退两难的境地中
+    2. on: 受到。。。支持的状态下
+      on drugs; -- 吃药
+      on a diet; -- 在节食
+    3. at: 在某个状态
+      at work;
+    4. under: 在(框架 前提)下
+      under construction; -- 在施工中
+      under discussion; -- 在讨论中
+  8. 表排除
+    1. besides: 除了。。。还
+      I like many things besides carrots;
+    2. except: 除了。。。(表示从整体中剔除出去)
+      Everyone went to the party except the rabbit;
+    3. except for: 除了(和except区分是 往往用来反驳前面的内容 相当于美中不足的是。。。)
+      the party is great except for the loud music;
+4. 介词短语(表完整含义，相当于副词)
+  1. 解释: 介词短语=介词+介词宾语
+  2. 做主语
+    between 10 and 11 is a good time for me;
+  3. 做表语
+     the meeting is at 2 oclock;
+  4. 做定语
+    this is the key to the house;
+  5. 做宾补
+    I found the carrot in good condition;
+  6. 做状语修饰动词(最常见)
+    the rabbit ate a carrot at noon;
+    the rabbit likes traveling by car;
+5. 介词固定搭配
+  1. 动词+介词=短语动词
+    check out -- 退房
+    make out -- 分辨
+    put out -- 熄灭
+    pass out -- 晕倒
+    work out -- 健身 
+    look out -- 小心
+    knock out -- 打晕
+  2. 例子
+    for example --比如
+    at ease -- 自在
+    in turn -- 依次
+    need for -- 需要。。。
+    long for -- 渴望
+    ready for -- 准备。。。
+
+
+
+
+
