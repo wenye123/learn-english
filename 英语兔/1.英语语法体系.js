c5be671ef900083b915a9c8643f613e936deb323
wenye <1583247975@qq.com>
2023-04-19 00:09:05 +0800

    更新

--- 英语兔/1.英语语法体系.js
+++ 英语兔/1.英语语法体系.js
@@ -257,6 +257,98 @@
           out of question; -- 毫无疑问
   3. 代词(pronouns): 代替人和物
   4. 数词(numerals): 表数量
+    1. 基数词: 数量的多少
+      1. 构成
+        1. 个位数: one two three four five six seven eight nine ten
+        2. 11-19: eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen
+        3. 整十数: twenty thirty forty fifty sixty seventy eighty ninety
+        4. 几十-几: 21(twenty-one) 97(ninety-seven)
+        5. 百: 100(one hundred) 200(two hundred)
+        6. 超过100的数字: 365(three hundred and sixty-five) 999(nine hundred and ninety-nine)
+        7. 上千到上亿: 
+          1000(one thousand) 2000(two thousand) 3546(three thousand five hundred and forty-six) 
+          1w(ten thousand) 10w(one hundred thousand) 100w(one million) 1000w(ten million) 1亿(one hundred million) 10亿(billion)
+          478,873,467,823(four hundred and seventy-eight billion eight hundred and seventy-three million four hundred and sixty-seven thousand eight hundred and twenty-three)
+      2. 充当成分
+        One and two is three; -- 主语 表语
+        It costs fifty; -- 宾语
+        There are two hundred students; -- 定语
+        You two can go with me; -- 同步语
+      3. 表不确定数量
+        1. hundreds of -- 数以百计
+        2. thousands of -- 数以千计
+          thousands of carrots; // 数以千计的胡萝卜
+          tens of thousands of carrots; // 数以万计的胡萝卜
+          hundreds of thousands of carrots; // 几十万根胡萝卜
+        3. millions of -- 数以百万
+    2. 序数词: 顺序的先后
+      1. 构成
+        1. 第1到19: 大部分后面+th 
+          特例: 1(first) 2(second) 3(third) 5(fifth) 8(eighth) 9(ninth) 12(twelfth)
+        2. 整10数: 把y改成ie 在加th: 20(twentieth)
+        3. 大数字: 22(twenty-second) 56(fifty-sixth) 101(one hundred and first);
+      2. 缩写形式: 
+        1. 第1-19： 阿拉伯数字+th: 13th
+          特例: 1(1st) 2(2nd) 3(3rd)
+        2. 其他数字: 阿拉伯数字+th: 13th
+          特例(1 2 3结尾): 21(21st) 52(52nd) 103(103rd)
+      3. 充当成分
+        1. 一般表示特指 需要加the
+          The fifth to pay the bill will get this prize; -- 主语
+          Please give me the second; -- 宾语
+          He is the 13th person to fall over this morning; -- 定语
+          She was the fitth to pay the bill; -- 表语
+        2. 不需要加the
+          this is my first book; -- 已经有my这个限定词 不需要加the
+        3. 类似基数词的表达
+          Room 315; page 200; the second lesson(第二节课); lesson 2(第二课时);
+    3. 用法
+      1. 分数: 基数词/序数词 分子大于1，分母的序数词还要+s变成复数, 带整数的用and连接
+        1/3(one third) 3/5(three fifths)
+        1/2(one second = a half) 1/4(one fourth = a quarter); 3/4(three fourths = three quarter)
+        2又1/3 -- two and one third;
+      2. 小数: 正常基数词+point+一个个的基数词
+        1.34(one point three four); 12.34(twelve point three four)
+      3. 百分数: 基数词+%
+        25%(twenty-five percent); 2.5%(two point five percent);
+      4. 倍数: half(一半) twice(两倍) three times(3倍);
+        1. 倍数 + the + 计量 + of;
+          This carrot is half/twice/three times the size of that one; // 这根胡萝卜是那根的一半/两倍/3倍
+        2. 倍数 + as。。。as。。。
+          I have half/twice/three times as many carrots as he has; // 我有的胡萝卜是他有的一半/两倍/3倍
+        3. 倍数+比较级+than;
+          I can eat three times faster than he can; // 我吃东西的速度比他快3倍
+        4. by+倍数 
+          I have increased my income by three times; // 我增加三倍收入
+      5. 数字符号和算术
+        2 plus 2 is 4 -- 2+2=4; 4 minus 2 is 2 -- 4-2=2; 2 multiplied by times 3 is 6 -- 2*3=6; 8 divided by 4 is 2 -- 8/4=2;
+        2 squared is 4 -- 2的平方; 2 cubed is 8 -- 2的3次方; 4 is more than 3 -- 4>3; 2 is less than 3 -- 2<3;
+      6. 单位
+        1. 时间
+          at seven -- 7:00; 
+          半点之前用past: (a quarter past seven)/(seven fifteen) -- 7:15; half past seven -- 7:30;
+          半点之后用to: (ten to eight)/(seven fifty) -- 7:50;
+        2. 日期: 日用的序数词 读音时候要把the读出来
+          1. 格式
+            1. 英式--日月年
+              12th December, 2020;
+            2. 美式--月日年;
+              December 12th, 2020;
+          2. 注意
+            500 B.C. -- 公元前500年; 500 A.D. -- 公元500年; 2020 -- 公元2020年(可不加AD);
+            2020''s/2020s -- 21世纪20年代; 1980''s/1980s -- 20世纪80年代;
+        3. 长度形容词
+            长: long/length;
+              The river is 1000 meters long; -- 基数词+单位词复数+形容词(长宽高); // 这条河有1000米长
+                = The river is 1000 meters in length; -- 基数词+单位词复数+in+形容词(长宽高);
+            宽: wide/width;
+            高: high/height;
+        4. 面积&体积
+          This room is 20 by 35 by 2 meters; // 这个房间是20米*35米*2米
+          This room is 700 square meters; // 这个房间有700平方米
+        5. 温度
+          37 degrees centigrade -- 37摄氏度; 37 degrees Fahrenheeit -- 37华氏度;
+          10 degrees below zero -- -10度;
   5. 形容词(adjectives): 形容人与物
   6. 副词(adverbs): 修饰动词或形容词
   7. 介词(prepositions): 虚词 表示和其他词关系的词
@@ -301,8 +393,6 @@
       3. 后位限定词: 量词 基数词 序数词
       3. 例子
         half my carrot; her last carrot; all ten carrots;
-
-
 
 
 句子成分: 8种句子成分
