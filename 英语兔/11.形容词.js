8fd5a9d1f5fbf1717da4d1f628c7330c2acca14e
wenye <1583247975@qq.com>
2023-04-22 10:26:27 +0800

    更新

---
+++ 英语兔/11.形容词.js
@@ -0,0 +1,101 @@
+1. 用法
+  1. 定语 
+    1. 前置定语: 限定词 + 主观特征 + 尺寸 + 形状 + 新旧 + 颜色 + 来源 + 材料 + 用途
+      a hungry rabbit; // 一只饿了的兔子
+      a little white rabbit; // 一只小白兔
+      a beautiful big round new black chinese wooden table; // 一张崭新的漂亮的黑色中式木质大圆桌
+    2. 后置定语: 修饰复合不定代词: somebody,nothing。。。
+      everything possible; nothing wrong;
+    3. 形容词短语做后置定语
+      Carrots good for the rabbit; // 对兔子有好处的胡萝卜
+      The rabbit eager to eat carrots; -- 形容词+动词不定式 // 急于吃胡萝卜的兔子
+        定语从句替换: The rabbit that is eager to eat carrots;
+      The rabbit busy eating carrots; -- 形容词+动名词 // 忙着吃胡萝卜的兔子
+        定语从句替换: The rabbit that is busy eating carrots;
+  2. 表语
+    1. 大部分形容词既能当定语也能当表语
+    2. 一些形容词只做表语: alive, asleep, alone, ill, well。。。
+      the rabbit is alive; -- 表语
+      an alive rabbit -- 不这么用
+        an living/sleeping/lonely/sick/healthy rabbit;
+      the only rabbit alive; -- 某些情况下可以做后置定语
+  3. 宾语
+    carrots make rabbits happy;
+  4. 状语
+    the rabbit came home, tired and hungry;
+
+2. 形容词的构成
+  1. 形容词+形容词/现在分词/过去分词
+    a red-hot coal; // 一块红热的煤
+    a good-looking rabbit; // 一只好看的兔子
+    a new-born baby; // 一个新生儿
+  2. 副词+形容词/现在分词/过去分词
+    all-round; // 全面的
+    hard-working; // 努力的
+    recently-built; // 最近建成的
+  3. 形容词/数词+名词
+    1. 形容人或物: 名词后面+ed -- 相当定语从句
+      a white-haired man; // 白发的
+        a man whose hair is white;
+      kind-hearted; // 好心的
+        ...whose heart is kind;
+    2. 其他
+      a long-distance call; // 长途电话
+      a one-way street; // 单行街道
+  4. 名词+形容词/现在分词/过去分词 -- 相当定语从句
+    a snow-white rabbit; // 一直雪白的兔子
+      a rabbit that is white like snow;
+    a carrot-eating rabbit; // 一直吃胡萝卜的兔子
+      a rabbit that eats carrots;
+    a heart-broken rabbit; // 一只心碎的兔子
+      a rabbit whose heart is broken;
+
+3. 比较级和最高级
+  1. 构成
+    1. 一般情况下 直接+er/est
+      long logner longest
+      tall taller tallest
+    2. e结尾 直接+r/st
+      late later latest
+      large larger largest
+    3. 辅音字母+y结尾: 把y改i再加er/est
+      happy happier happiest
+      busy busier busiest
+    4. 重读闭音节单词 双写词尾辅音字母 + er/est 
+      big bigger biggest
+      fat fatter fattest
+    5. more/most + 多音节词/部分双音节词 
+      interesting -> more interesting -> most interesting
+      difficult -> more difficult -> most difficult
+    6. 不规则变化
+      good better best 
+      bad worse worst 
+      many more most 
+      little less least
+  2. 用法 
+    1. 比较级
+      1. 。。。比。。。更。。。
+        this carrot is bigger than that one; 
+        this carrot is three times bigger than that one;
+      2. 。。和。。一样。。(这里无需比较级)
+        this carrot is as big as that one;
+        this carrot is three times as big as that one;
+      3. 越来越。。
+        prettier and prettier; // 越来越漂亮
+        more and more beautiful; -- 这里省略第一个more
+      4. 越。。就越。。: the+比较级(原因)+补充内容, the+比较级(结果)+补充内容
+        the more carrots the rabbit eats, the smarter he will become; -- 表假设 // 兔子吃胡萝卜越多 就会变得越聪明
+        the more carrots the rabbit ate, the smarter he became; -- 表过去 // 兔子吃了越来越多胡萝卜 变得越来越聪明
+        the more, the better; -- 省略语境 // 越多越好
+    2. 最高级(需要加the表特指)
+      1. in+地点 of+所属范围
+        Magic mirror on the wall, whol is the fairest one of all;
+      2. 其他
+        snow white is the most beautiful woman in the world;     
+        the queen is a most beautiful woman; -- 这里的most表示非常 不是最高级 所以前面可以用a
+
+
+
+
+
+
